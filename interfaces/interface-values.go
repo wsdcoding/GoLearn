d2791a687d15b98a587e85a451e155fcca8f8ef1
wsdcoding <weishide1@gamil.com>
2025-02-24 18:51:05 +0800

    commit 2.24

---
+++ interfaces/interface-values.go
@@ -0,0 +1,17 @@
+/**
+* @File  : interface-values.go
+* @Author: Shide 2025/1/18 19:56
+* @Desc  : 接口值
+	接口值可以用作函数的参数或者返回值
+2.24测试github
+*/
+
+package main
+
+type Iinterface_values interface {
+	M()
+}
+
+type Tinterface_values struct {
+	S string
+}
